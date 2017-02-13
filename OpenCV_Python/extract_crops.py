24a985e965f2805ff1c3177b702e541cf6fd9866
pablovicente <pablovicente93@usal.es>
2017-02-13 09:00:09 -0500

    Both modes activate

--- OpenCV_Python/extract_crops.py
+++ OpenCV_Python/extract_crops.py
@@ -8,6 +8,11 @@ import datetime
 import numpy as np
 from os import walk, path
 
+
+GRND_IMAGES = 1
+CROP_IMAGES = 2
+BOTH_IMAGES = 3
+
 def check_for_files(base_path):
 
     f = []
@@ -15,11 +20,16 @@ def check_for_files(base_path):
         f.extend(filenames)
         break
 
-    a = [int(re.split('[_.]',filename)[1]) for filename in filenames if '.jpg' in filename]
-    return max(a)
+    a = [int(re.split('[_.]',filename)[2]) for filename in filenames if '.jpg' in filename]
+
+    if len(a) == 0: return 0
+    else: return max(a) + 1
+    
+    
 
 
 def main():
+
     # python bg_substractor.py --video ../videos/GOPR0195.MP4 
     # construct the argument parser and parse the arguments
     ap = argparse.ArgumentParser()
@@ -28,6 +38,7 @@ def main():
     ap.add_argument("-t", "--type", default="cars", help="type of sample to capture")
     ap.add_argument("-i", "--index", type=int, default=0, help="name_index")
     ap.add_argument("-d", "--dir", default="../samples/", help="directory to store samples")
+    ap.add_argument("-m", "--mode", type=int, default=3, help="directory to store samples")
 
     args = vars(ap.parse_args())
 
@@ -44,8 +55,9 @@ def main():
     fgbg = cv2.bgsegm.createBackgroundSubtractorMOG()
 
 
+    mode = args["mode"]
     base_path = args["dir"]
-    base_name = args["type"] + '_' 
+    base_name = args["type"]
     index = check_for_files(base_path)
 
     # loop over the frames of the video
@@ -62,6 +74,10 @@ def main():
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         gray = cv2.GaussianBlur(gray, (21, 21), 0)
 
+
+        # read frame to get image features
+        height, width, channels = frame.shape
+
         # apply background substractor mask to frame
         fgmask = fgbg.apply(frame)
         open_kern = np.ones((5,5), np.uint8)
@@ -73,41 +89,59 @@ def main():
         (_, cnts, _) = cv2.findContours(opening.copy(), cv2.RETR_EXTERNAL, 
             cv2.CHAIN_APPROX_SIMPLE)    
 
+
+        # keep the original image to obtain cropped portions
         original = frame.copy()
+        # create black image to later save ground truth
+        ground_truth = np.zeros((height,width,3), np.uint8)
+
         for i, cnt in enumerate(cnts):
             # if the contour is too small, ignore it
             if 200<cv2.contourArea(cnt)<5000:
 
                 (x, y, w, h) = cv2.boundingRect(cnt)
                 #cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-                cv2.drawContours(frame,[cnt],0,(0,255,0),2)            
+                cv2.drawContours(frame,[cnt],0,(0,255,0),2)
 
-
+                if GRND_IMAGES == mode or BOTH_IMAGES == mode:                                    
+                    cv2.drawContours(ground_truth,[cnt],0,(0,255,0),
+                        thickness=cv2.FILLED)
+                    
         cv2.imshow("Ground", frame)    
         cv2.imshow("Segmentation", opening)
-
 
         key = cv2.waitKey(1) & 0xFF 
         # if the `q` key is pressed, break from the lop
         if key == ord("q"):        
             break
 
+        # if the `s` key is pressed, save either cropped images or ground truth
         if key == ord("s"):
             screenshot_samples = 0
-            for i, cnt in enumerate(cnts):
+            subindex = 0
+
+            # loop over each contour and get the cropped image
+            if CROP_IMAGES == mode or BOTH_IMAGES == mode:                                    
+                for i, cnt in enumerate(cnts):
                 # if the contour is too small, ignore it
-                if 200<cv2.contourArea(cnt)<5000:
-                    # (x, y, w, h) = cv2.boundingRect(cnt)                
-                    height,width,channels = frame.shape
-                    ground_truth = np.zeros((height,width,3), np.uint8)
-                    cv2.drawContours(ground_truth,[cnt],0,(0,255,0),thickness=cv2.FILLED)
-                    cv2.imwrite(base_path + base_name + str(index) + ".jpg", ground_truth)
-                    #cv2.imwrite(base_path + base_name + str(index) + ".jpg", original[y:y + h,x:x + w])
-                    index += 1
+                    if 200<cv2.contourArea(cnt)<5000:                    
+                    
+                        # save the cropped image
+                        subindex += 1
+                        (x, y, w, h) = cv2.boundingRect(cnt)                
+                        cv2.imwrite(base_path + base_name + '_cropped_' + str(index) + 
+                                '_' + str(subindex) + ".jpg", original[y:y + h,x:x + w])                    
+
+                        screenshot_samples += 1
 
 
-                screenshot_samples += 1
-            print("%d taken" % screenshot_samples)
+            if GRND_IMAGES == mode or BOTH_IMAGES == mode:                                                    
+                cv2.imwrite(base_path + base_name + '_bg_' + str(index) + ".jpg", ground_truth)
+
+            cv2.imwrite(base_path + base_name + '_ground_'+ str(index) + ".jpg", frame)
+            index += 1
+
+            print("Screenshot taken")
 
     camera.release()
     cv2.destroyAllWindows()
